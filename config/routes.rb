015d2fab713d9d27933dfa01b809bb19856a4dbb
Nantiwat Thongseng <nwatth@gmail.com>
2019-06-08 22:56:33 +0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'login', to: redirect('/auth/line'), as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get 'auth/line/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+
   root 'home#index'
 end
